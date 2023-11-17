9d13c653704a69c49ba460aec25993af1b1bd7fb
(Student  C00294397) Aleksandra Skarzycka <C00294397@setu.ie>
2023-11-17 09:37:36 +0000

    printMenu function

--- git4/git4/converter.cpp
+++ git4/git4/converter.cpp
@@ -8,3 +8,17 @@ int main()
 {
 	return 1;
 }
+
+char printMenu()
+{
+	char choice = ' '; // user input choice
+	std::cout << "Please enter an upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+
+	std::cout << "[D] Mpg    <--->    L/100km [d]" << std::endl;
+	std::cout << "[E] Kilograms    <--->    Stone & Pounds [e]" << std::endl;
+	std::cout << "[F] Celsius    <--->    Fahrenheit [f]" << std::endl;
+	std::cout << "[Q] Exit    <--->    Exit [q]" << std::endl;
+	std::cout << "Enter your choice:";
+	std::cin >> choice;
+	return choice;
+}

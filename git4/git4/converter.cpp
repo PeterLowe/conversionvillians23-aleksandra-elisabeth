6f78c27d0fb55e0d21c5703b7ade3a9a2da44ca9
(Student  C00301316) Elisabeth Sykorova <C00301316@setu.ie>
2023-11-17 09:41:13 +0000

    printMenu A-C

--- git4/git4/converter.cpp
+++ git4/git4/converter.cpp
@@ -3,7 +3,25 @@
 #include<iostream>
 #include<string>
 
+char printMenu();
+
 int main()
 {
+	char choice = ' ';
+	choice = printMenu();
 	return 1;
+}
+
+char printMenu()
+{
+	char choice = ' '; // user input
+	std::cout << "Please enter en upper case leter to convert from left to right\nand lower case letter to convert from right to left\n\n" << std::endl;
+	std::cout << "[A]		Degrees		<--->		Radians		[a]" << std::endl;
+	std::cout << "[B]		Centimeters	<--->		Feet & Inches	[b]" << std::endl;
+	std::cout << "[C]		Litres		<--->		Gallons		[c]" << std::endl;
+	std::cout << "[Q]		Exit		[---]		Exit		[q]" << std::endl;
+
+	std::cout << "Enter your choice: ";
+	std::cin >> choice;
+	return choice;
 }
